0d4403bf2c23152b22a7419b994b81ecf20436f4
Promise-Digitals <promiseedeh025@gmail.com>
2024-10-17 20:48:22 +0100

    signup and login API

--- backend/index.js
+++ backend/index.js
@@ -130,6 +130,85 @@ app.get('/allproducts' , async (req, res) => {
 })
 
 
+// Schema creating for User Model
+const Users = mongoose.model('Users', {
+    name: {
+        type: String
+    },
+    email: {
+        type: String,
+        unique: true
+    },
+    password: {
+        type: String
+    },
+    cartData: {
+        type: Object
+    },
+    date: {
+        type: Date,
+        default: Date.now()
+    }
+})
+
+
+// Creating endpoint for registering the user
+app.post('/signup', async (req, res) => {
+    let check = await Users.findOne({email: req.body.email});
+
+    if(check){
+        return res.status(400).json({success: false, errors: "User with same email already exist"})
+    }
+
+    let cart = {};
+    for(let i = 0; i < 300; i++){
+        cart[i] = 0;
+    }
+    
+    const user = new Users({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+        cartData: cart
+    })
+
+    await user.save();
+
+    const data = {
+        user: {
+            id: user.id
+        }
+    }
+
+    const token = jwt.sign(data, 'secret_string');
+    res.json({success: true, token})
+})
+
+// Creating endpoint for user login
+app.post('/login', async (req, res) => {
+    let user = await Users.findOne({email: req.body.email});
+    if(user){
+        const passCompare = req.body.password === user.password;
+
+        if(passCompare) {
+            const data = {
+                user: {
+                    id: user.id
+                }
+            }
+            const token = jwt.sign(data, 'secret_string');
+            res.json({success: true, token})
+        }else{
+            res.json({success: false, errors: "Wrong Password"})
+        }
+    }else{
+        res.json({success: false, errors: "User does not exist"})
+    }
+})
+
+
+
+
 
 app.listen(port, (error) => {
     if(!error){
