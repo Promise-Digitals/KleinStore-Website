af5e7127d68d372b437a1804e254de45b7cecce0
Promise-Digitals <promiseedeh025@gmail.com>
2024-10-17 07:51:22 +0100

    completed admin panel

--- admin/src/Components/ListProduct/ListProduct.jsx
+++ admin/src/Components/ListProduct/ListProduct.jsx
@@ -16,6 +16,18 @@ const ListProduct = () => {
         fetchInfo()
     }, [])
 
+    const removeProduct = async (id) => {
+        await fetch('http://localhost:4000/removeproduct', {
+            method: 'POST',
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body:JSON.stringify({id: id})
+        })
+        await fetchInfo();
+    }
+
     return (
         <div className='list-product'>
             <h1>All Products List</h1>
@@ -27,22 +39,25 @@ const ListProduct = () => {
                 <p>Category</p>
                 <p>Remove</p>
             </div>
+            
 
             <div className="listproduct-allproducts">
                 <hr />
                 {allProducts.map((product, index) => {
-                    return <div key={index} className="listproduct-format-main listproduct-format">
+                    return <><div key={index} className="listproduct-format-main listproduct-format">
                         <img src={product.image} alt="" className='listproduct-product-icon'/>
                         <p>{product.name}</p>
                         <p>${product.old_price}</p>
                         <p>${product.new_price}</p>
                         <p>{product.category}</p>
-                        <img src={cross_icon} alt="" className='listproduct-remove-icon'/>
+                        <img src={cross_icon} alt="" className='listproduct-remove-icon' onClick={()=>{removeProduct(product.id)}}/>
                     </div>
+                    <hr />
+                    </>
                 })}
             </div>
         </div>
     )
 }
 
-export default ListProduct
+export default ListProduct;
